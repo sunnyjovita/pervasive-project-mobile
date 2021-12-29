6feb3ed07f38e7f62d356c73c52dc45dac123edf
Sunny Jovita <sunnyjovitaaa@gmail.com>
2021-12-29 16:25:32 +0700

    first commit

--- pervasiveproject/lib/screens/splash/components/body.dart
+++ pervasiveproject/lib/screens/splash/components/body.dart
@@ -19,28 +19,12 @@ class _BodyState extends State<Body>{
       children: <Widget>[
         Expanded(
           flex: 3, // length of the box
-          child: Column(
-            children: <Widget>[
-              Spacer(),
-              Text(
-                "Lock&Go", 
-                style: TextStyle(
-                  fontSize: getProportionateScreenWidth(36), // title size
-                  color: kPrimaryColor,
-                  fontWeight: FontWeight.bold,
-              ),
-              ),
-              Text("Welcome to Lock&Go, have you locked your door?"),
-              Spacer(flex: 2),
-              Image.asset(
-                "./assets/images/splash_1.png",
-              //   // height: getProportionateScreenHeight(265),
-              //   // width: getProportionateScreenWidth(235),
-              ),
-            ],
-        
-        ),
-        ),
+          child: PageView.builder( // slider page
+            itemBuilder: (context, index) => SplashContent(
+            image: "./assets/images/remote-control-door-lock.png",
+            text: "Welcome to Lock&Go, have you locked your door?",
+          ),
+      )),
       Expanded(
         flex: 2,
         child: SizedBox(),
@@ -50,5 +34,37 @@ class _BodyState extends State<Body>{
     ),
     );
 
+  }
+}
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({
+    Key? key, required this.text, required this.image, 
+  }) : super(key: key);
+  final String text, image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Spacer(),
+        Text(
+          "Lock&Go", 
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(36), // title size
+            color: kPrimaryColor,
+            fontWeight: FontWeight.bold,
+        ),
+        ),
+        Text(text),
+        Spacer(flex: 1),
+        Image.asset(
+          image,
+          height: getProportionateScreenHeight(265),
+          // width: getProportionateScreenWidth(235),
+        ),
+      ],
+        
+        );
   }
 }
