44bd2768912078e0e6a83ad552a3c3ed71bf610f
Sunny Jovita <sunnyjovitaaa@gmail.com>
2021-12-29 15:03:48 +0700

    Update body.dart

--- pervasiveproject/lib/screens/splash/components/body.dart
+++ pervasiveproject/lib/screens/splash/components/body.dart
@@ -13,19 +13,29 @@ class _BodyState extends State<Body>{
 
     // change safearea to move the title
     return SafeArea(
+      child: SizedBox(
+        width: double.infinity, // move words into middle
       child: Column(
       children: <Widget>[
         Expanded(
           flex: 3, // length of the box
           child: Column(
             children: <Widget>[
+              Spacer(),
               Text(
                 "Lock&Go", 
                 style: TextStyle(
-                  fontSize: getProportionateScreenWidth(36),
+                  fontSize: getProportionateScreenWidth(36), // title size
                   color: kPrimaryColor,
                   fontWeight: FontWeight.bold,
               ),
+              ),
+              Text("Welcome to Lock&Go, have you locked your door?"),
+              Spacer(flex: 2),
+              Image.asset(
+                "./assets/images/splash_1.png",
+              //   // height: getProportionateScreenHeight(265),
+              //   // width: getProportionateScreenWidth(235),
               ),
             ],
         
@@ -36,6 +46,7 @@ class _BodyState extends State<Body>{
         child: SizedBox(),
         ),
       ],
+    ),
     ),
     );
 
