6af164c2b67df0228d3437150a02e51792c68952
Sunny Jovita <sunnyjovitaaa@gmail.com>
2021-12-30 21:24:55 +0700

    4th commit

--- pervasiveproject/lib/screens/sign_in/components/body.dart
+++ pervasiveproject/lib/screens/sign_in/components/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:pervasiveproject/theme.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pervasiveproject/constant.dart';
+// import 'package:pervasiveproject/theme.dart';
 
 import '../../../size_config.dart';
 
@@ -7,21 +9,63 @@ class Body extends StatelessWidget {
   @override 
   Widget build(BuildContext context){
     return SafeArea(
+      child: SizedBox(
+        // make the welcome back ang sign in with... in the center
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: getProportionateScreenWidth(20), 
+          ),
+          child: Column(
+            children:[
+              Text(
+                "Welcome Back",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: getProportionateScreenWidth(28),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "Sign in with your email and password \nor continue with social media",
+                textAlign: TextAlign.center,
+              ),
+              SignForm(),
+            ], 
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignForm extends StatefulWidget {
+  // const ({ Key? key }) : super(key: key);
+  @override
+  _SignFormState createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
       child: Column(
-        children:[
-          Text(
-            "Welcome Back",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: getProportionateScreenWidth(28),
-              fontWeight: FontWeight.bold,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: "Email",
+              hintText: "Enter your email",
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+
+              // create icon email 
+              // suffixIcon: SvgPicture.asset("pervasiveproject/assets/icons/Mail.svg"),
+
             ),
           ),
-          Text(
-            "Sign in with your email and password \nor continue with social media"
-          ),
-        ], 
+
+      ],
       ),
+      
     );
   }
 }
--- pervasiveproject/lib/theme.dart
+++ pervasiveproject/lib/theme.dart
@@ -9,8 +9,29 @@ ThemeData theme(){
         fontFamily: "Montserrat",
         appBarTheme: appBarTheme(),
         textTheme: textTheme(),
+        inputDecorationTheme: inputDecorationTheme(),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       );
+}
+
+InputDecorationTheme inputDecorationTheme() {
+  OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+              borderRadius: BorderRadius.circular(28),
+              borderSide: BorderSide(color: kTextColor),
+              gapPadding: 10,
+            );
+  return InputDecorationTheme(
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 42,
+              vertical: 20,
+            ),
+            enabledBorder: outlineInputBorder,
+            focusedBorder: outlineInputBorder,
+            border: outlineInputBorder,
+
+
+
+          );
 }
 
 TextTheme textTheme(){
