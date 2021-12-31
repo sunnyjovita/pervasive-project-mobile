143d34d556c0cb56a7bbf0417043909fd4fd48f6
Sunny Jovita <sunnyjovitaaa@gmail.com>
2021-12-31 15:13:43 +0700

    update login

--- pervasiveproject/lib/screens/sign_in/components/body.dart
+++ pervasiveproject/lib/screens/sign_in/components/body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pervasiveproject/components/socal_card.dart';
+import 'package:pervasiveproject/constant.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 // import 'package:pervasiveproject/components/default_button.dart';
 // import 'package:pervasiveproject/components/form_error.dart';
 // import 'package:pervasiveproject/constant.dart';
@@ -20,28 +23,75 @@ class Body extends StatelessWidget {
           padding: EdgeInsets.symmetric(
             horizontal: getProportionateScreenWidth(20), 
           ),
-          child: Column(
-            children:[
-              Text(
-                "Welcome Back",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: getProportionateScreenWidth(28),
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              children:[
+                 SizedBox(height: SizeConfig.screenHeight * 0.04),
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                "Sign in with your email and password \nor continue with social media",
-                textAlign: TextAlign.center,
-              ),
-              SignForm(),
-            ], 
+                Text(
+                  "Sign in with your email and password \nor continue with social media",
+                  textAlign: TextAlign.center,
+                ),
+          
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+          
+                SignForm(),
+          
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                
+                // for social media icons
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocalCard( // google
+                      icon: "./assets/icons/google-icon.svg",
+                      press: (){},  
+                    ),
+                    SocalCard( // facebook
+                      icon: "./assets/icons/facebook-2.svg",
+                      press: (){},  
+                    ),
+                    SocalCard( // twitter
+                      icon: "./assets/icons/twitter.svg",
+                      press: (){},  
+                    ),
+                  ],
+                ),
+          
+                SizedBox(height: getProportionateScreenHeight(20)),
+          
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account? ",
+                      style: TextStyle(
+                        fontSize: getProportionateScreenWidth(16),
+                      ),
+                    ),
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: getProportionateScreenWidth(16),
+                        color: kPrimaryColor,
+                      ),
+                    )
+                  ],
+                ),
+          
+              ], 
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
 
