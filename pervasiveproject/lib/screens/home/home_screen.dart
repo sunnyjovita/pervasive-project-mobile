c2768e8f67da765bf2114eea129994a9ad5d8628
Sunny Jovita <sunnyjovitaaa@gmail.com>
2022-01-15 19:43:19 +0700

    third commit

--- pervasiveproject/lib/screens/home/home_screen.dart
+++ pervasiveproject/lib/screens/home/home_screen.dart
@@ -7,23 +7,26 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: AppBar(
         centerTitle: true,
         title: Text(
           "Lock and Go", 
           style: TextStyle(
-            color: Color(0XFF8B8B8B), 
+            // color: Color(0XFF8B8B8B), 
+            color: Colors.black,
             fontSize: 18,
+            fontWeight: FontWeight.bold,
             ),
             ),
         iconTheme: IconThemeData(color: Colors.white),
         leading: Image.asset(
-          'assets/lock1.png',
-          width: 35,
-          height: 35,
-          ),
+          "assets/images/lock3.png",
+          // width: 100,
+          // height: 100,
+        ),
+        ),
 
-      ),
       body: Body(),
     );
   }
