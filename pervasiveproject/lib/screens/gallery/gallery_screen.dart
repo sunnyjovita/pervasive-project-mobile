96e9eb90a8c0ae95f77b9854ce5d1335c7071276
Sunny Jovita <sunnyjovitaaa@gmail.com>
2022-01-20 02:42:58 +0700

    update final done

--- pervasiveproject/lib/screens/gallery/gallery_screen.dart
+++ pervasiveproject/lib/screens/gallery/gallery_screen.dart
@@ -26,62 +26,65 @@ class _GalleryScreenState extends State<GalleryScreen> {
             ),
       ),
 
-      body: new FutureBuilder(
+      body: Column(
+        children: <Widget>[
+          Image.network("https://i.imgur.com/CmWA8kf.png"), 
+        ],
+      )
+      // new FutureBuilder(
 
-        future: getPics(),
-        builder: (context, AsyncSnapshot snapShot)
-        {
-          print('Building with snapshot = $snapShot');
-            List data = snapShot.data;
-            // print(data[0]['url']);
-            if(snapShot.hasError){
-              print(snapShot.error);
-              return Text('Failed to get response from the server',
-              style: TextStyle(color: Colors.red,
-              fontSize: 22.0),
-              );
+        // future: getPics(),
+        // builder: (context, AsyncSnapshot snapShot)
+        // {
+        //   print('Building with snapshot = $snapShot');
+        //     List data = snapShot.data;
+        //     // print(data[0]['url']);
+        //     if(snapShot.hasError){
+        //       print(snapShot.error);
+        //       return Text('Failed to get response from the server',
+        //       style: TextStyle(color: Colors.red,
+        //       fontSize: 22.0),
+        //       );
 
-            }else if(snapShot.hasData){
-              return new Center(
-                child: new ListView.builder(
-                  itemCount: data.length,
-                  itemBuilder: (context, index){
-                    return new Column(
-                      children: <Widget> [
-                        new Padding(padding: const EdgeInsets.all(5.0)),
-                        new Container(
-                          child: new InkWell(
-                            child: new Image.network(
-                              // '${data["hits"][index]['userImageURL']}'
-                               '${data[index]['download_url']}'
-                               
-                            ),
-                          ),
-                        )
-                      ],
-                    );
+        //     }else if(snapShot.hasData){
+        //       return new Center(
+        //         child: new ListView.builder(
+        //           itemCount: data.length,
+        //           itemBuilder: (context, index){
+        //             return new Column(
+        //               children: <Widget> [
+        //                 new Padding(padding: const EdgeInsets.all(5.0)),
+        //                 new Container(
+        //                   child: new InkWell(
+        //                     child: new Image.network(
+        //                        '${data[index]['download_url']}'
+        //                     ),
+        //                   ),
+        //                 )
+        //               ],
+        //             );
                     
-                  }
+        //           }
                  
-                  ),
-              );
-            }else{
-              return new Center(child: CircularProgressIndicator()); 
-            }
+        //           ),
+        //       );
+        //     }else{
+        //       return new Center(child: CircularProgressIndicator()); 
+        //     }
             
        
-        }),
+        // }),
     );
   }
 }
-var apiKey = '9443505-db0b1bdc3709bafd4c6cebb87';
-Future<List> getPics() async{
-  var url = Uri.parse('https://picsum.photos/v2/list');
-  // var url = Uri.parse('https://pixabay.com/api/?key=$apiKey&q=cars&image_type=photo&pretty=true');
-  http.Response response = await http.get(url);
-  return json.decode(response.body);
+// var apiKey = '9443505-db0b1bdc3709bafd4c6cebb87';
+// Future<List> getPics() async{
+//   // var url = Uri.parse('https://picsum.photos/v2/list');
+//   var url = Uri.parse('https://i.imgur.com/CmWA8kf.png');
+//   http.Response response = await http.get(url);
+//   return json.decode(response.body);
 
-}
+// }
 
 
 
